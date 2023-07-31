078f8e9cfd012a94be65a617124242e05f207e00
pablosaenzar <pablo.saenz@device42.com>
2023-07-31 13:05:55 -0500

    Product Documentation and two Solution guides

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
           <div className={styles.cardContainerSolution}>
             <h2>Solution Guides</h2>
 
-            <div className={styles.cardsContainer}>
+            <div className={styles.cardsContainerSolution}>
               {solutionGuideData.map((card, idx) => (
                 <ContentCard key={idx} {...card} />
               ))}
